551753d5445a65e8918ab419e4fdb281f86e1419
PhamHongVan98 <hongvanhongvan98@gmail.com>
2023-11-09 10:59:24 +0700

    em nộp BT buổi 3 ạ

---
+++ src/BTVNbuoi3/chitieu.java
@@ -0,0 +1,16 @@
+package BTVNbuoi3;
+
+public class chitieu {
+        public String tenkhoanchi;
+        public int xeploaikhoanchi;
+        public String douutien;
+        public float sotiencanchi;
+
+        }
+
+
+
+
+
+
+
---
+++ src/BTVNbuoi3/main.java
@@ -0,0 +1,25 @@
+package BTVNbuoi3;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main (String[] args){
+
+        chitieu ct= new chitieu();
+        Scanner sc= new Scanner(System.in);
+        System.out.printf("Nhập ten khoan chi: ");
+        ct.tenkhoanchi= sc.nextLine();
+        System.out.printf("Nhập xep loai: ");
+        ct.xeploaikhoanchi= sc.nextInt();
+        sc.nextLine();
+        System.out.printf("Nhập độ ưu tiên: ");
+        ct.douutien=sc.nextLine();
+        System.out.printf("Nhập Số tiền: ");
+        ct.sotiencanchi= sc.nextFloat();
+        System.out.println("Khoản chi: "+ ct.tenkhoanchi);
+        System.out.println("Xếp loại khoản chi: "+ ct.xeploaikhoanchi);
+        System.out.println("Độ ưu tiên: "+ ct.douutien);
+        System.out.print("Số tiền cần chi: "+ ct.sotiencanchi);
+
+    }
+}
