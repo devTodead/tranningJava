53a09af55193aa4a1a513d69e102734da8ac6c64
hthang1 <hoangthuhangftu@gmail.com>
2023-11-09 14:44:51 +0700

    HTHANG1 NỘP LẠI BÀI TẬP BUỔI 3

--- src/lesson2/hthang/main.java
+++ src/lesson2/hthang/main.java
@@ -1,4 +1,5 @@
 package lesson2.hthang;
+import java.text.DecimalFormat;
 import java.util.Scanner; //BÀI TẬP BUỔI 3
 public class main {
     public static void main (String[] args){
@@ -24,7 +25,7 @@ public class main {
         System.out.println("TÊN KHOẢN CHI = " + tien.tenKhoanChi);
         System.out.println("ĐỘ ƯU TIÊN = " + tien.doUuTien);
         System.out.println("XẾP LOẠI KHOẢN CHI = "+ tien.xepLoaiKhoanChi);
-        System.out.println("SỐ TIỀN CẦN CHI = "+tien.soTienCanChi);
-
+        DecimalFormat format = new DecimalFormat("###,###,###");
+        System.out.println("SỐ TIỀN CẦN CHI = : " + (format.format(tien.soTienCanChi)) + " VNĐ");
     }
 }
