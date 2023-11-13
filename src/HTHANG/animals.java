b840046d3e770adf2d4c8c736be9b2366262569d
NVHAI <ngohai.dev@gmail.com>
2023-11-14 05:42:29 +0700

    thêm demo bài luyện tập

--- src/HTHANG/animals.java
+++
@@ -1,9 +0,0 @@
-package HTHANG;
-
-public class animals {
-    public int soChan;
-    public String tenDongVat;
-    public void toilaai(){
-        System.out.println("Hải dớ là " + tenDongVat);
-    }
-}
