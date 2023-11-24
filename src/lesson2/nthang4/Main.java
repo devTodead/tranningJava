2c75c0ed0f5f1d4ccd97e8b4b3f3532364570f6a
nthang4 <thanhhang8820@gmail.com>
2023-11-24 11:45:14 +0700

    Em nộp bài Luyện tập chức năng 1,2,3,4,5 ạ

--- src/lesson2/nthang4/Main.java
+++ src/lesson2/nthang4/Main.java
@@ -4,12 +4,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner Spending = new Scanner(System.in);
+        /*Scanner Spending = new Scanner(System.in);
         Spending CT = new Spending();
         CT.Ten_khoan_chi =Spending.nextLine();
         CT.Loai_khoan_chi=Spending.nextLine();
         CT.Do_uu_tien=Spending.nextLine();
         CT.So_tien=Spending.nextInt();
         CT.Ham();
+         */
+        Luyentap LT = new Luyentap();
+        LT.CN1();
+        LT.CN2();
+        LT.CN3();
+        LT.CN4();
+        LT.CN5();
+       // LT.CN6();
     }
 }
