d04302a795e7f33694424694a1f9dfd053acbae5
hthang1 <hoangthuhangftu@gmail.com>
2023-11-27 14:02:43 +0700

    HTHANG1 NỘP BÀI TẬP L3 (1-7)

--- src/lesson2/hthang/input.java
+++ src/lesson2/hthang/input.java
@@ -60,14 +60,14 @@ public class input {
 
         //tìm UCLN
         for (int i = x; i > 1; i--) { //gán giá trị khởi tạo bằng x hoặc y, chạy lùi
-            if (x % i == 0 && y % i == 0) { //nếu cả x và y đều chia hết cho i => i là UCLN
+            if (x % i == 0 & y % i == 0) { //nếu cả x và y đều chia hết cho i => i là UCLN
                 System.out.println("UCLN của " + x + " và " + y + " là: " + i);
                 break;
             }
         }
         //tìm BCNN
         for (int i = 2; ; i++) { //biểu thức điều kiện trống, vô hạn
-            if (i % x == 0 && i % y == 0) {
+            if (i % x == 0 & i % y == 0) {
                 System.out.println("BCNN của " + x + " và " + y + " là: " + i);
                 break;
             }
@@ -76,7 +76,7 @@ public class input {
 
     public void Unit3() { // tính tiền karaoke
         double donGia = 150000;
-        int Vao = 0, Ra = 0 , TongGio;
+        int Vao = 0, Ra = 0, TongGio;
         int tongGio;
         double tienThanhToan = 0;
         double khuyenMai; //km từ giờ thứ 4
@@ -91,7 +91,7 @@ public class input {
         Ra = scanner.nextInt();
         tongGio = Ra - Vao;
 
-        if (Vao < 12 || Ra > 23){
+        if (Vao < 12 || Ra > 23 || Vao >= Ra) {
             System.out.println(" Giờ không hợp lệ");
         } else if (tongGio > 3) {
             khuyenMai = (tongGio - 3) * 0.3 * donGia;
@@ -105,7 +105,7 @@ public class input {
     }
 
     public void Unit4() { //tính tiền điện
-        float soDien, tienDien = 0 ;
+        float soDien, tienDien = 0;
         int bac1 = 1678;
         int bac2 = 1734;
         int bac3 = 2014;
@@ -141,4 +141,104 @@ public class input {
         System.out.println(" Tiền điện = " + (format.format(tienDien)) + " VNĐ");
     }
 
+    public void Unit5() {
+        int soTien;
+        int[] menhGia = {500, 200, 100, 50, 20, 10, 5, 2, 1};
+        int soToTien;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap vao so tien can doi: ");
+        soTien = sc.nextInt();
+        System.out.println("So tien can doi " + soTien + " gom: ");
+        sc.nextLine();
+
+        if (soTien < 0) {
+            System.out.println("So tien khong hop le");
+
+        } else {
+            for (int i = 0; i < 9; i++) {
+                soToTien = soTien / menhGia[i];
+                if (soToTien > 0) { //!=: khac
+                    System.out.println("menh gia " + menhGia[i] + " co " + soToTien + " to");
+                }
+                soTien = soTien - soToTien * menhGia[i];
+            }
+        }
+    }
+    public void Unit6 (){
+            double soTienMuonVay = 0 ;
+            double tienLaiHangThang, tienPhaiTraHangThang, soTienConLai;
+
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("Nhap vao so tien muon vay: ");
+            soTienMuonVay = sc.nextDouble();
+            double tienGocPhaiTraHangThang = soTienMuonVay / 12;
+            soTienConLai = soTienMuonVay;
+            for (int i = 1; i <=12; i++){
+                tienLaiHangThang = soTienConLai * 0.05;
+                tienPhaiTraHangThang = tienGocPhaiTraHangThang + tienLaiHangThang;
+                soTienConLai = soTienConLai - tienGocPhaiTraHangThang;
+                DecimalFormat format = new DecimalFormat("###,###,###");
+                System.out.println("-----------");
+                System.out.println("ky han " + i);
+                System.out.println("Tien lai phai tra = " + (format.format(tienLaiHangThang)) + " VND ");
+                System.out.println("Tien goc phai tra = " + (format.format(tienGocPhaiTraHangThang)) + " VND ");
+                System.out.println("Tong so tien phai tra hang thang = "+ (format.format(tienPhaiTraHangThang)) + " VND ");
+                System.out.println("So tien con lai = " + (format.format(soTienConLai)) + " VND ");
+            }
+        }
+    public void Unit7() {
+        double tienXe;
+        double tienVay;
+        double tienTraTruoc;
+        double soPhanTramVay;
+        double soPhanTramTraTruoc;
+        double tienGocPhaiTraHangThang, soTienConLai, tienLaiHangThang, tienPhaiTraHangThang;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap vao so tien xe: ");
+        tienXe = sc.nextDouble();
+        System.out.println("Nhap vao so phan tram vay: ");
+        soPhanTramVay = sc.nextDouble();
+        soPhanTramTraTruoc = 1 - soPhanTramVay/100;
+        tienTraTruoc = tienXe * soPhanTramTraTruoc;
+        tienVay = tienXe * soPhanTramVay;
+        if (tienVay > 500000000) {
+            tienVay = 500000000;
+            DecimalFormat format = new DecimalFormat("###,###,###");
+            System.out.println("So tien phai tra lan dau = " + (format.format(tienTraTruoc)) + " VND ");
+            tienGocPhaiTraHangThang = tienVay / 24;
+            soTienConLai = tienVay;
+            for (int i = 1; i <= 24; i++) {
+                tienLaiHangThang = soTienConLai * 0.072;
+                tienPhaiTraHangThang = tienGocPhaiTraHangThang + tienLaiHangThang;
+                soTienConLai = soTienConLai - tienGocPhaiTraHangThang;
+                System.out.println("-----------");
+                System.out.println("ky han " + i);
+                System.out.println("Tien lai phai tra = " + (format.format(tienLaiHangThang)) + " VND ");
+                System.out.println("Tien goc phai tra = " + (format.format(tienGocPhaiTraHangThang)) + " VND ");
+                System.out.println("Tong so tien phai tra hang thang = " + (format.format(tienPhaiTraHangThang)) + " VND ");
+                System.out.println("So tien con lai = " + (format.format(soTienConLai)) + " VND ");
+            }
+        } else {
+            tienVay = tienXe - tienTraTruoc;
+            System.out.println("So tien phai tra lan dau = " + tienTraTruoc);
+            tienGocPhaiTraHangThang = tienVay / 24;
+            soTienConLai = tienVay;
+            for (int i = 1; i <= 24; i++) {
+                tienLaiHangThang = soTienConLai * 0.072;
+                tienPhaiTraHangThang = tienGocPhaiTraHangThang + tienLaiHangThang;
+                soTienConLai = soTienConLai - tienGocPhaiTraHangThang;
+                DecimalFormat format = new DecimalFormat("###,###,###");
+                System.out.println("-----------");
+                System.out.println("ky han " + i);
+                System.out.println("Tien lai phai tra = " + (format.format(tienLaiHangThang)) + " VND ");
+                System.out.println("Tien goc phai tra = " + (format.format(tienGocPhaiTraHangThang)) + " VND ");
+                System.out.println("Tong so tien phai tra hang thang = " + (format.format(tienPhaiTraHangThang)) + " VND ");
+                System.out.println("So tien con lai = " + (format.format(soTienConLai)) + " VND ");
+            }
+        }
+    }
 }
+
